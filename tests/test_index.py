e136889bf111b591429893820036ae8436d7f89d
Thiyagu55 <thiyagu.nataraj@searce.com>
2022-09-06 11:45:00 +0530

    Added tests

--- tests/test_index.py
+++ tests/test_index.py
@@ -1,4 +1,4 @@
-# Copyright The OpenTelemetry Authors
+# Copyright Searce
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,5 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-if __name__ == "__main__":
-    print("Hello")
+
+def test_add():
+    assert True
