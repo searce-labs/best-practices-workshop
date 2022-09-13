2f31fdbbc153e755da7fdb166034a1a67a8cfa35
Thiyagu55 <thiyagu.nataraj@searce.com>
2022-09-13 16:31:25 +0530

    division operation

--- index.py
+++ index.py
@@ -4,5 +4,16 @@
 # license that can be found in the LICENSE file or at
 # https://opensource.org/licenses/MIT.
 
+
+def division(num1, num2):
+    """
+    Perform division
+    :param num1: int
+    :param num2: int
+    :return: int
+    """
+    return num1/num2
+
+
 if __name__ == "__main__":
     print("Hello")
